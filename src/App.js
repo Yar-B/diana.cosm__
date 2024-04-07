fcb78e1604aca9efbe78694ceda60b90ee24d8f4
Yaroslav Bryanskiy <yaroslav.bry@mail.ru>
2024-04-07 16:01:31 +0400

    added price list page and fixes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,32 @@
+import { useState } from 'react'
+import { useEffect } from 'react'
 import './App.css'
+import ErrorSizePage from './pages/ErrorSizePage'
 import FirstPage from './pages/FirstPage'
+import PriceListFirstPage from './pages/PriceListFirstPage'
 import SecondPage from './pages/SecondPage'
 
 function App() {
+	const [clientWindowSize, setClientWindowSize] = useState(0)
+	window.addEventListener('resize', () => {
+		setClientWindowSize(document.body.clientHeight / document.body.clientWidth)
+	})
+	useEffect(() => {
+		setClientWindowSize(document.body.clientHeight / document.body.clientWidth)
+	}, [])
 	return (
 		<>
-			<FirstPage />
-			<SecondPage />
+			{clientWindowSize > 1.5 ? (
+				<>
+					<FirstPage />
+					<SecondPage />
+					<PriceListFirstPage />
+				</>
+			) : (
+				<>
+					<ErrorSizePage />
+				</>
+			)}
 		</>
 	)
 }
---
+++ src/pages/ErrorSizePage.js
@@ -0,0 +1,15 @@
+function ErrorSizePage() {
+	return (
+		<>
+			<div className='page-wrapper'>
+				<div className='error-message-container'>
+					<div className='phone-icon'></div>
+					<p>Этот сайт предназначен для вертикального использования на мобильном устройстве</p>
+				</div>
+			</div>
+		</>
+	)
+}
+
+export default ErrorSizePage
+
