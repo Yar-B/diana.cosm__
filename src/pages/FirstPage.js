3e5d7f28ec155fa3a11044c804d35b6cc5369bf4
Yaroslav Bryanskiy <yaroslav.bry@mail.ru>
2024-04-21 19:56:21 +0400

    added content for contact page

--- src/pages/FirstPage.js
+++ src/pages/FirstPage.js
@@ -7,18 +7,20 @@ function FirstPage() {
 	return (
 		<>
 			<div className='page-wrapper'>
-				<div className='first-page'>
-					<div className='first-page-title'>
-						<div className='title'>
-							<p>ТВОЙ</p>
-							<p>КОСМЕТОЛОГ</p>
+				<div className='first-page-bg'>
+					<div className='first-page'>
+						<div className='first-page-title'>
+							<div className='title'>
+								<p>ТВОЙ</p>
+								<p>КОСМЕТОЛОГ</p>
+							</div>
+							<div className='description'>
+								<p>Мастер эстетической и инъекционной косметологии - Диана</p>
+							</div>
+							<button onClick={() => goToContacts()} className='button'>
+								Связаться
+							</button>
 						</div>
-						<div className='description'>
-							<p>Мастер эстетической и инъекционной косметологии - Диана</p>
-						</div>
-						<button onClick={() => goToContacts()} className='button'>
-							Связаться
-						</button>
 					</div>
 				</div>
 			</div>
--- src/pages/PriceListPage.js
+++ src/pages/PriceListPage.js
@@ -3,27 +3,7 @@ function PriceListPage() {
 		<>
 			<div className='price-list-page-wrapper'>
 				<div className='price-list-page'>
-					<div className='title'>PRICE LIST</div>
-					<div className='price-list-card'>
-						<h2>Комплексы</h2>
-						<div className='service-row'>
-							<p>Очищающий комплекс лица + пилинг</p>
-							<span>3500/3700</span>
-						</div>
-						<div className='service-row'>
-							<p>Классический массаж лица + альгинатная маска</p>
-							<span>2600</span>
-						</div>
-						<div className='service-row'>
-							<p>Профессиональный уход за жирной кожей лица + дарсонваль</p>
-							<span>3200</span>
-						</div>
-						<div className='service-row'>
-							<p>Профессиональный уход лица + альгинатная маска</p>
-							<span>3000</span>
-						</div>
-					</div>
-
+					<div className='title'>ПРАЙС-ЛИСТ</div>
 					<div className='price-list-card'>
 						<h2>Массаж лица</h2>
 						<div className='service-row'>
@@ -61,6 +41,26 @@ function PriceListPage() {
 						<div className='service-row'>
 							<p>Биоревитализация "Hyaron"</p>
 							<span>4000</span>
+						</div>
+					</div>
+
+					<div className='price-list-card'>
+						<h2>Комплексы</h2>
+						<div className='service-row'>
+							<p>Очищающий комплекс лица + пилинг</p>
+							<span>3500/3700</span>
+						</div>
+						<div className='service-row'>
+							<p>Классический массаж лица + альгинатная маска</p>
+							<span>2600</span>
+						</div>
+						<div className='service-row'>
+							<p>Профессиональный уход за жирной кожей лица + дарсонваль</p>
+							<span>3200</span>
+						</div>
+						<div className='service-row'>
+							<p>Профессиональный уход лица + альгинатная маска</p>
+							<span>3000</span>
 						</div>
 					</div>
 				</div>
